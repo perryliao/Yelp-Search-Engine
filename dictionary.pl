e8926b30666012e1f1bfe06bb6419e75f7b42210
Cynzy <cynthiazhou98@gmail.com>
2021-04-04 23:02:42 -0700

    added more terms to dictionary

--- dictionary.pl
+++ dictionary.pl
@@ -1,12 +1,10 @@
-:- module(dictionary, [starter_phrase/2]).
-
 starter_phrase(['What', 'is' | P], P).
 starter_phrase(['What', '\'', 's' | T], T).
 starter_phrase([Imperative | T], T) :-
     imperative(Imperative).
-starter_phrase([Imperative, Asker | T], T) :-
+starter_phrase([Imperative, Pronoun | T], T) :-
     imperative(Imperative),
-    asker(Asker).
+    pronoun(Pronoun).
 starter_phrase(['I', 'want' | P], P).
 starter_phrase(['I', 'want', 'to', 'eat' | P], P).
 starter_phrase('I\'m', 'feeling', 'lucky').
@@ -20,32 +18,59 @@ imperative('recommend').
 imperative('Find').
 imperative('find').
 
-
-asker('me').
-asker('us').
+pronoun('me').
+pronoun('us').
 
 % ex. the cheapest Mexican restaurant in Vancouver
-restaurant_description(P0, P8, Entity, C0, C8) :-
+restaurant_description(P0, P7, Entity, C0, C7) :-
     det(P0, P1, Entity, C0, C1),
     adj(P1, P2, Entity, C1, C2),             % one adjective only
     category(P2, P3, Entity, C2, C3),        % one category only
     keyword(P3, P4, Entity, C3, C4),         % remaining terms
     prep(P4, P5, Entity, C4, C5),
-    location(P5, P6, Entity, C5, C6).
+    det(P5, P6, Entity, C5, C6),
+    location(P6, P7, Entity, C6, C7).
 
 det(['a' | P], P, _, C, C).
 det(['an' | P], P, _, C, C).
 det(['the' | P], P, _, C, C).
 det(P, P, _, C, C).
 
-adj(['cheapest' | P], P, _, [price(1, 'LessThan')|C], C).
-adj(['cheap' | P], P, _, [price(2, 'LessThan')|C], C).
-adj(['fancy' | P], P, _, [price(3, 'GreaterThan'), rating(4, 'GreaterThan') |C], C).
-adj(['expensive' | P], P, _, [price(4, 'EqualTo') |C], C).
-adj(['good' | P], P, _, [rating(3, 'GreaterThan') |C], C).
-adj(['great' | P], P, _, [rating(4, 'GreaterThan') |C], C).
-adj(['amazing' | P], P, _, [rating(5, 'EqualTo') |C], C).
-adj(['best' | P], P, _, [rating(5, 'EqualTo') |C], C).
-adj(['worst' | P], P, _, [rating(1, 'EqualTo') |C], C).
+adj(['cheapest' | P], P, _, [price('1')|C], C).
+adj(['cheap' | P], P, _, [price('1,2')|C], C).
+adj(['fancy' | P], P, _, [price('4'), rating('4,5') |C], C).
+adj(['expensive' | P], P, _, [price('4') |C], C).
+adj(['good' | P], P, _, [rating('3,4,5') |C], C).
+adj(['great' | P], P, _, [rating('4,5') |C], C).
+adj(['amazing' | P], P, _, [rating('5') |C], C).
+adj(['best' | P], P, _, [rating('5') |C], C).
+adj(['worst' | P], P, _, [rating('1') |C], C).
 adj(P, P, _, C, C).
 
+keyword(P3, P4, _, C, C)
+keyword(P, P, _, C, C).
+
+prep(['in' | P], P, _, C, C).
+prep(['of' | P], P, _, C, C).
+prep(['on' | P], P, _, C, C).
+prep(['at' | P], P, _, C, C).
+prep(['by' | P], P, _, C, C).
+prep(['down' | P], P, _, C, C).
+prep(['within' | P], P, _, C, C).
+prep(P, P, _, C, C).
+
+location([L| P], P, L, C, C) :- isCanada(L).
+location([L| P], P, L, C, C) :- inCanada(L).
+location(['near me' | P], P, _, C, C).
+location(['area' | P], P, _, C, C).
+location(['city' | P], P, _, C, C).
+location(['block' | P], P, _, C, C).
+location(['community' | P], P, _, C, C).
+location(['neighborhood' | P], P, _, C, C).
+location(['Canada' | P], P, _, C, C).
+location(['canada' | P], P, _, C, C).
+
+% isCanada(C) is true if C is Canada
+isCanada('Canada').
+isCanada('canada').
+isCanada('can').
