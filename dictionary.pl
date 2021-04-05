47389ade65241e69cc4095add506eeef7774f7b7
Cynzy <cynthiazhou98@gmail.com>
2021-04-04 19:33:17 -0700

    added more dictionary terms

--- dictionary.pl
+++ dictionary.pl
@@ -1,4 +1,4 @@
-:- module(dictionary, [starter_phrase/2).
+:- module(dictionary, [starter_phrase/2]).
 
 starter_phrase(['What', 'is' | P], P).
 starter_phrase(['What', '\'', 's' | T], T).
@@ -21,3 +21,28 @@ imperative('find').
 
 asker('me').
 asker('us').
+
+% ex. the cheapest Mexican restaurant in Vancouver
+restaurant_description(P0, P8, Entity, C0, C8) :-
+    det(P0, P1, Entity, C0, C1),
+    adj(P1, P2, Entity, C1, C2),
+    category(P2, P3, Entity, C2, C3),
+    keyword(P3, P4, Entity, C3, C4),
+    prep(P4, P5, Entity, C4, C5),
+    location(P5, P6, Entity, C5, C6).
+
+det(['a' | P], P, _, C, C).
+det(['an' | P], P, _, C, C).
+det(['the' | P], P, _, C, C).
+det(P, P, _, C, C).
+
+quality_adj(['cheapest' | P], P, _, [price(1, 'LessThan')|C], C).
+quality_adj(['cheap' | P], P, _, [price(2, 'LessThan')|C], C).
+quality_adj(['fancy' | P], P, _, [price(3, 'GreaterThan'),rating(4, 'GreaterThan') |C], C).
+quality_adj(['expensive' | P], P, _, [price(4, 'EqualTo') |C], C).
+quality_adj(['good' | P], P, _, [rating(3, 'GreaterThan') |C], C).
+quality_adj(['great' | P], P, _, [rating(4, 'GreaterThan') |C], C).
+quality_adj(['amazing' | P], P, _, [rating(5, 'EqualTo') |C], C).
+quality_adj(['best' | P], P, _, [rating(5, 'EqualTo') |C], C).
+quality_adj(['worst' | P], P, _, [rating(1, 'EqualTo') |C], C).
+quality_adj(P, P, _, C, C).
