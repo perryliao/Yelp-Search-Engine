12a22df5f29e6d492d1e6450b1b02fe9fd39dda7
Cynzy <cynthiazhou98@gmail.com>
2021-04-04 19:41:58 -0700

    added more dictionary terms

--- dictionary.pl
+++ dictionary.pl
@@ -25,9 +25,9 @@ asker('us').
 % ex. the cheapest Mexican restaurant in Vancouver
 restaurant_description(P0, P8, Entity, C0, C8) :-
     det(P0, P1, Entity, C0, C1),
-    adj(P1, P2, Entity, C1, C2),
-    category(P2, P3, Entity, C2, C3),
-    keyword(P3, P4, Entity, C3, C4),
+    adj(P1, P2, Entity, C1, C2),             % one adjective only
+    category(P2, P3, Entity, C2, C3),        % one category only
+    keyword(P3, P4, Entity, C3, C4),         % remaining terms
     prep(P4, P5, Entity, C4, C5),
     location(P5, P6, Entity, C5, C6).
 
@@ -38,7 +38,7 @@ det(P, P, _, C, C).
 
 adj(['cheapest' | P], P, _, [price(1, 'LessThan')|C], C).
 adj(['cheap' | P], P, _, [price(2, 'LessThan')|C], C).
-adj(['fancy' | P], P, _, [price(3, 'GreaterThan'),rating(4, 'GreaterThan') |C], C).
+adj(['fancy' | P], P, _, [price(3, 'GreaterThan'), rating(4, 'GreaterThan') |C], C).
 adj(['expensive' | P], P, _, [price(4, 'EqualTo') |C], C).
 adj(['good' | P], P, _, [rating(3, 'GreaterThan') |C], C).
 adj(['great' | P], P, _, [rating(4, 'GreaterThan') |C], C).
@@ -46,3 +46,4 @@ adj(['amazing' | P], P, _, [rating(5, 'EqualTo') |C], C).
 adj(['best' | P], P, _, [rating(5, 'EqualTo') |C], C).
 adj(['worst' | P], P, _, [rating(1, 'EqualTo') |C], C).
 adj(P, P, _, C, C).
+
