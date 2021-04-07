5a2b2ff2bfe57d2ef1b038ba924f2dc7ad7a80ca
Cynzy <cynthiazhou98@gmail.com>
2021-04-06 22:29:14 -0700

    added smart query

--- main.pl
+++ main.pl
@@ -4,7 +4,19 @@
 askUser() :-
     write("Search for Yelp Restaurants: "),
     flush_output(current_output),
-    readln(Ln).
+    readln(Ln),
+    query(Ln, Constraints),
+    search(Constraints, Result),
+    return_results(Result).
+
+% Parse regular restaurant query
+query(P0, Constraints) :-
+    query_head(P0, P1),
+    restaurant_query(P1, _, _, Constraints, _).
+
+% Parse Im feeling lucky smart restaurant query
+query(['I\'m', 'feeling', 'lucky'], Constraints) :-
+    smart_query(Constraints).
 
 
 % Request response from Yelp API for category list
