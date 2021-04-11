dce6d99a642a92615b60aa4b5fcb88648e4ca98f
Cynzy <cynthiazhou98@gmail.com>
2021-04-11 01:12:34 -0700

    fixed loop bug and adjusted dictionary

--- dictionary.pl
+++ dictionary.pl
@@ -4,9 +4,6 @@ query_head(['What', 'is' | P], P).
 query_head(['What', '\'', 's' | T], T).
 query_head([Imperative | T], T) :-
     imperative(Imperative).
-query_head([Imperative, Pronoun | T], T) :-
-    imperative(Imperative),
-    pronoun(Pronoun).
 query_head(['I', 'want' | P], P).
 query_head(['I', 'want', 'to', 'eat' | P], P).
 query_head(P, P).
@@ -17,64 +14,50 @@ imperative('Suggest').
 imperative('suggest').
 imperative('Recommend').
 imperative('recommend').
-imperative('Find').
-imperative('find').
 
-pronoun('me').
-pronoun('us').
 
 % ex. the cheapest Mexican restaurant in Vancouver
-restaurant_query(P0, P7, Entity, C0, C7) :-
-    det(P0, P1, Entity, C0, C1),
-    adj(P1, P2, Entity, C1, C2),             % one adjective only
-    category(P2, P3, Entity, C2, C3),        % one category only
-    keyword(P3, P4, Entity, C3, C4),         % remaining terms
-    prep(P4, P5, Entity, C4, C5),
-    det(P5, P6, Entity, C5, C6),
-    location(P6, P7, Entity, C6, C7).
+restaurant_query(P0, P7, C0, C7) :-
+    det(P0, P1, C0, C1),
+    adj(P1, P2, C1, C2),             % one adjective only
+    category(P2, P3, C2, C3),        % one category only
+    keyword(P3, P4, C3, C4),         % remaining terms
+    prep(P4, P5, C4, C5),
+    det(P5, P6, C5, C6),
+    location(P6, P7, C6, C7).
 
-det(['a' | P], P, _, C, C).
-det(['an' | P], P, _, C, C).
-det(['the' | P], P, _, C, C).
-det(P, P, _, C, C).
+det(['a' | P], P, C, C).
+det(['an' | P], P, C, C).
+det(['the' | P], P, C, C).
+det(P, P, C, C).
 
-adj(['cheapest' | P], P, _, [price('1')|C], C).
-adj(['cheap' | P], P, _, [price('1,2')|C], C).
-adj(['fancy' | P], P, _, [price('4'), rating('4,5') |C], C).
-adj(['expensive' | P], P, _, [price('4') |C], C).
-adj(['good' | P], P, _, [rating('3,4,5') |C], C).
-adj(['great' | P], P, _, [rating('4,5') |C], C).
-adj(['amazing' | P], P, _, [rating('5') |C], C).
-adj(['best' | P], P, _, [rating('5') |C], C).
-adj(['worst' | P], P, _, [rating('1') |C], C).
-adj(P, P, _, C, C).
+adj(['cheapest' | P], P, [price('1')|C], C).
+adj(['cheap' | P], P, [price('1,2')|C], C).
+adj(['fancy' | P], P, [price('4'), rating('4,5') |C], C).
+adj(['expensive' | P], P, [price('4') |C], C).
+adj(['good' | P], P, [rating('3,4,5') |C], C).
+adj(['great' | P], P, [rating('4,5') |C], C).
+adj(['amazing' | P], P, [rating('5') |C], C).
+adj(['best' | P], P, [rating('5') |C], C).
+adj(['worst' | P], P, [rating('1') |C], C).
+adj(P, P, C, C).
 
-keyword(P3, P4, _, [term([P3|P4])], _). % TODO: change to [term([P3|P4]) | C], C ?
-keyword(P, P, _, C, C).
-
-prep(['in' | P], P, _, C, C).
-prep(['of' | P], P, _, C, C).
-prep(['on' | P], P, _, C, C).
-prep(['at' | P], P, _, C, C).
-prep(['by' | P], P, _, C, C).
-prep(['down' | P], P, _, C, C).
-prep(['within' | P], P, _, C, C).
-prep(P, P, _, C, C).
-
-location(['near', 'me' | P], P, _, [location('nearby') | C], C).
-location(['area' | P], P, _, [location('nearby')| C], C).
-location(['city' | P], P, _, [location('nearby')| C], C).
-location(['block' | P], P, _, [location('nearby')| C], C).
-location(['community' | P], P, _, [location('nearby')| C], C).
-location(['neighborhood' | P], P, _, [location('nearby')| C], C).
-location([L | P], P, L, [location(L)| C], C) :-
-    dif(L, 'near me'),
-    dif(L, 'area'),
-    dif(L, 'city'),
-    dif(L, 'block'),
-    dif(L, 'community'),
-    dif(L, 'neighborhood').
-
-category([L | P], P, L, [category(L)], _) :- % TODO: change to [category(L) | C], C ?
+category([L | P], P, [category(L)| C], C) :-
     set_categories(Categories),
     member_open(L, Categories).
+
+keyword(['restaurant' | P], P, C, C).
+keyword(['food' | P], P, C, C).
+keyword(['place' | P], P, C, C).
+keyword(P, P, C, C).
+
+location([L | P], P, [location(L)| C], C).
+
+prep(['in' | P], P, C, C).
+prep(['of' | P], P, C, C).
+prep(['on' | P], P, C, C).
+prep(['at' | P], P, C, C).
+prep(['by' | P], P, C, C).
+prep(['down' | P], P, C, C).
+prep(['within' | P], P, C, C).
+prep(P, P, C, C).
--- main.pl
+++ main.pl
@@ -7,19 +7,18 @@ askUser() :-
     flush_output(current_output),
     readln(Ln),
     query(Ln, Constraints),
-    search(Constraints, Result),
-    return_results(Result).
+    search(Constraints, Results),
+    return_results(Results).
 
 % Parse regular restaurant query
-% ex. query(['tacos','in','vancouver'], Constraints).
 query(P0, Constraints) :-
     query_head(P0, P1),
-    dif(P0, ['I\'m', 'feeling', 'lucky']),
-    restaurant_query(P1, _, _, Constraints, _).
+    dif(P0, ['I','\'','m', 'feeling', 'lucky']),
+    restaurant_query(P1, _, Constraints, _).
 
 % Parse Im feeling lucky smart restaurant query
-query(['I\'m', 'feeling', 'lucky'], Constraints) :-
-    smart_query(Constraints).
+% query(['I','\'','m', 'feeling', 'lucky'], Constraints) :-
+ %   smart_query(Constraints).
 
 % Search Yelp Database and make API call
 search(Constraints, Results) :-
